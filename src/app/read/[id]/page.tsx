62a25612fb072b68294dec000b902a66647aeaf9
Leo <drd9811@naver.com>
2023-08-13 15:44:43 +0900

    백엔드 데이터 읽어오기

--- src/app/read/[id]/page.tsx
+++ src/app/read/[id]/page.tsx
@@ -6,11 +6,14 @@ interface Props {
     };
 }
 
-const Read: FC<Props> = (props) => {
+const Read: FC<Props> = async (props) => {
+    const response  = await fetch(`http://localhost:9999/topics/${props.params.id}`);
+    const topic: {id: number, title: string, body: string} = await response.json();
+
     return (
       <>
-          <h2>Read Component!</h2>
-          parameters : {props.params.id}
+          <h2>{topic.title}</h2>
+          {topic.body}
       </>
     );
 }
