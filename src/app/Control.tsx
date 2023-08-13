5712606c93fe23fc670f639e46adf7345f922efb
Leo <drd9811@naver.com>
2023-08-13 16:29:32 +0900

    백엔드 데이터 삭제하기

--- src/app/Control.tsx
+++ src/app/Control.tsx
@@ -2,10 +2,22 @@
 
 import React from "react";
 import Link from "next/link";
-import {useParams} from "next/navigation";
+import {useParams, useRouter} from "next/navigation";
 
 export const Control = () => {
     const params = useParams();
+    const router = useRouter();
+
+    const deleteButtonHandle = () => {
+          fetch('http://localhost:9999/topics/' + params.id, {
+              method: 'DELETE'
+          }).then((response) => {
+              return response.json();
+          }).then((topic) => {
+              router.push('/');
+              router.refresh();
+          });
+    };
 
     return (
         <>
@@ -13,7 +25,7 @@ export const Control = () => {
                 <li><Link href='/create'>Create</Link></li>
                 {params.id ? <>
                     <li><Link href={'/update/' + params.id}>Update</Link></li>
-                    <li><input type={'button'} value='delete' /></li>
+                    <li><input type={'button'} value='delete' onClick={deleteButtonHandle}/></li>
                 </> : null}
             </ul>
         </>
